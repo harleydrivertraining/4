53039274e3ea03723fa37a543e004c6e7ac59549
harleydrivertraining <117176931+harleydrivertraining@users.noreply.github.com>
2022-11-01 22:03:21 +0000

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,39 @@
-// ignore_for_file: prefer_const_constructors
+// Developer: Samuel Adekunle
+// YouTube Tutorial: https://youtube.com/playlist?list=PLMfrNHAjWCoB6roLO1soz6RMc5BdnU9pk
+// Join the community on Discord: https://discord.com/invite/NytgTkyw3R
+// Let's connect on Twitter: https://twitter.com/techwithsam_
 
-import 'dart:async';
-
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:prime_web/screens/home_screen.dart';
-import 'package:prime_web/screens/settings_screen.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'homepage.dart';
 
-import '../provider/navigationBarProvider.dart';
-import '../screens/splash_screen.dart';
-import '../helpers/Constant.dart';
-import '../screens/main_screen.dart';
-import '../widgets/admob_service.dart';
-import '../provider/theme_provider.dart';
-import 'screens/main_screen_bottom_bar.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-final navigatorKey = GlobalKey<NavigatorState>();
-
-AndroidNotificationChannel channel = const AndroidNotificationChannel(
-    'msg_channel', // id
-    'High Notifications', // title
-    description:
-        'This channel is used for important notifications.', // description
-    importance: Importance.high,
-    playSound: true);
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-Future main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); //when we have to communicate to flutter framework before initializing app
-
-  if (showInterstitialAds) {
-    AdMobService.initialize();
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
+  await FlutterDownloader.initialize(debug: true);
+  // set true to enable printing logs to console
+  await Permission.storage.request();
+  await Permission.mediaLibrary.request();
+  // ask for storage permission on app create
 
-  flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          IOSFlutterLocalNotificationsPlugin>()
-      ?.requestPermissions(
-        alert: true,
-        badge: true,
-        sound: true,
-      );
-
-  bool isNavVisible = true;
-  int counter = 0;
-  SharedPreferences.getInstance().then((prefs) {
-    prefs.setInt('counter', counter);
-    var isDarkTheme =
-        prefs.getBool("isDarkTheme") ?? ThemeMode.system == ThemeMode.dark
-            ? true
-            : false;
-    return runApp(
-      ChangeNotifierProvider<ThemeProvider>(
-        child: MyApp(),
-        create: (BuildContext context) {
-          return ThemeProvider(isDarkTheme);
-        },
-      ),
-    );
-  });
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  @override
-  void initState() {
-    super.initState();
-    if (showSplashScreen) {
-      startTimer();
-    }
-  }
-
-  startTimer() async {
-    var duration = Duration(seconds: 3);
-    return Timer(
-        duration,
-        () => navigatorKey.currentState!.pushReplacement(MaterialPageRoute(
-            builder: (_) =>
-                showBottomNavigationBar ? MainScreen() : MyHomePage())));
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-      /* start--commnet  below 2 lines to enable landscape mode */
-      DeviceOrientation
-          .landscapeLeft, 
-      DeviceOrientation
-          .landscapeRight 
-         /*end */
-    ]);
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<NavigationBarProvider>(
-            create: (_) => NavigationBarProvider())
-      ],
-      child: Consumer<ThemeProvider>(builder: (context, value, child) {
-        return MaterialApp(
-          title: appName,
-          debugShowCheckedModeBanner: false,
-          themeMode: value.getTheme(),
-          theme: AppThemes.lightTheme,
-          darkTheme: AppThemes.darkTheme,
-          navigatorKey: navigatorKey,
-          home: showSplashScreen
-              ? SplashScreen()
-              : showBottomNavigationBar
-                  ? MainScreen()
-                  : MyHomePage(),
-          onGenerateRoute: (RouteSettings settings) {
-            switch (settings.name) {
-              case 'home':
-                return MaterialPageRoute(builder: (_) => HomeScreen());
-              case 'settings':
-                return MaterialPageRoute(builder: (_) => SettingsScreen());
-            }
-          },
-        );
-      }),
+    return MaterialApp(
+      title: 'TechWithSam - Flutter Webview Tutorial',
+      theme: ThemeData(
+          primarySwatch: Colors.blue, visualDensity: VisualDensity.standard),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
