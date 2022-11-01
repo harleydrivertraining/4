adcbcc8f13390007f53a90a7fe9fead26f42a7d4
harleydrivertraining <117176931+harleydrivertraining@users.noreply.github.com>
2022-11-01 22:14:10 +0000

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 
-void main() => runApp(App());
+import 'features/presentation/pages/shop_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Himdeve Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ShopPage(),
+    );
+  }
+}
